3b6f544cfa471cb1cad235242cf72f3ad37f6327
gustavv0 <00001123502043sp@al.educacao.sp.gov.br>
2024-10-21 07:31:17 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'viagem dos sonhos',
+    'paris'
+)
+
+criaCartao(
+    'pessoal',
+    'filme favorito',
+    'scarface'
+)
+
+criaCartao(
+    'recomendação',
+    'animal favorito?',
+    'cachorro'
+)
+
+criaCartao(
+    'recomendacão',
+    'passa tempo favorito?',
+    'assistir videos'
+)
